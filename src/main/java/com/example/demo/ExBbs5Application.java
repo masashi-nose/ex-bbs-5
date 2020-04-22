63ce0c4dcd5892516a83ea809c01bdf974f27254
nosemasashi <ma.nose9413@gmail.com>
2020-04-22 11:32:53 +0900

    ArticleとCommentドメインを作成しました。:w

---
+++ src/main/java/com/example/demo/ExBbs5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBbs5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBbs5Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/domain/Article.java
@@ -0,0 +1,59 @@
+package com.example.demo.domain;
+
+import java.util.List;
+
+/**
+ * articlesテーブルのエンティティです.
+ * 
+ * @author masashi.nose
+ *
+ */
+public class Article {
+
+	/** 記事ID */
+	private Integer id;
+	/** 名前 */
+	private String name;
+	/** 投稿内容 */
+	private String content;
+	/** コメントリスト */
+	private List<Comment> commentList;
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Comment> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(List<Comment> commentList) {
+		this.commentList = commentList;
+	}
+
+}
